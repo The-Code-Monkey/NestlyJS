2b8714368df6684d84e74d4495b52f630025ccd7
Andy Wilson <andy.wilson1234@gmail.com>
2025-09-18 00:18:52 +0100

    Update devServer.ts

--- packages/server/src/devServer.ts
+++ packages/server/src/devServer.ts
@@ -1,16 +1,31 @@
 import cluster from "cluster";
-import path from "path";
 
 export function start({ port = 3000 }: { port?: number }) {
   if (cluster.isMaster) {
     console.log(`Dev Master ${process.pid} is running`);
 
-    cluster.fork({ ...process.env, WORKER_TYPE: "ssr", PORT: port });
-    cluster.fork({ ...process.env, WORKER_TYPE: "api", PORT: port });
+    const workers: Record<string, cluster.Worker> = {};
+
+    const forkWorker = (type: "ssr" | "api") => {
+      const worker = cluster.fork({
+        ...process.env,
+        WORKER_TYPE: type,
+        PORT: port.toString(),
+      });
+      workers[worker.id] = worker;
+      return worker;
+    };
+
+    // Fork two workers
+    forkWorker("ssr");
+    forkWorker("api");
 
     cluster.on("exit", (worker) => {
-      console.log(`Worker ${worker.process.pid} died. Restarting...`);
-      cluster.fork({ ...process.env, WORKER_TYPE: worker.process.env.WORKER_TYPE, PORT: port });
+      console.log(`Worker ${worker.process?.pid} died. Restarting...`);
+
+      // Lookup the type by id, fallback to "ssr"
+      const type = Object.entries(workers).find(([, w]) => w.id === worker.id)?.[1].process?.env.WORKER_TYPE || "ssr";
+      forkWorker(type);
     });
   } else {
     const type = process.env.WORKER_TYPE;
