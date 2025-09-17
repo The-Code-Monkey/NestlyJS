18f8f021df7d9aaefef51760cdfe4015f08fe6c8
Andy Wilson <andy.wilson1234@gmail.com>
2025-09-17 22:54:51 +0100

    Update prodServer.ts

--- packages/server/src/prodServer.ts
+++ packages/server/src/prodServer.ts
@@ -1,25 +1,31 @@
 import cluster from "cluster";
 import os from "os";
 
-const numCPUs = Math.min(os.cpus().length, 4);
+export function start({
+  port = 3000,
+  totalWorkers = Math.max(os.cpus().length, 4),
+}: { port?: number; totalWorkers?: number }) {
+  if (cluster.isMaster) {
+    console.log(`Prod Master ${process.pid} is running`);
+    console.log(`Forking ${totalWorkers} workers...`);
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+    const apiWorkersCount = Math.max(1, Math.floor(totalWorkers / 4));
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({ WORKER_TYPE: i === numCPUs - 1 ? "api" : "ssr" });
-  }
+    for (let i = 0; i < totalWorkers; i++) {
+      cluster.fork({
+        WORKER_TYPE: i < apiWorkersCount ? "api" : "ssr",
+        PORT: port,
+      });
+    }
 
-  cluster.on("exit", (worker) => {
-    console.log(`Worker ${worker.process.pid} died. Restarting...`);
-    const type = worker.process.env.WORKER_TYPE || "ssr";
-    cluster.fork({ WORKER_TYPE: type });
-  });
-} else {
-  const workerType = process.env.WORKER_TYPE;
-  if (workerType === "api") {
-    require("./apiWorker");
+    cluster.on("exit", (worker) => {
+      console.log(`Worker ${worker.process.pid} died. Restarting...`);
+      const type = worker.process.env.WORKER_TYPE || "ssr";
+      cluster.fork({ WORKER_TYPE: type, PORT: port });
+    });
   } else {
-    require("./ssrWorker");
+    const type = process.env.WORKER_TYPE;
+    if (type === "api") require("./apiWorker");
+    else require("./ssrWorker");
   }
 }
