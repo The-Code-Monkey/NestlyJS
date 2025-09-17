748ac6b713ca53af4420f794a33533cfb2a74159
Andy Wilson <andy.wilson1234@gmail.com>
2025-09-17 22:39:32 +0100

    Create apiChild.ts

---
+++ packages/server/src/apiChild.ts
@@ -0,0 +1,13 @@
+process.on("message", async (msg) => {
+  const { url, method, headers } = msg;
+
+  // Example API response
+  const response = {
+    message: "Hello from API child",
+    url,
+    method,
+    headers
+  };
+
+  process.send({ body: response, status: 200 });
+});
