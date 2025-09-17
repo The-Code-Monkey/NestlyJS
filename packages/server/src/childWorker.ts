713c803db46e6667188a402a4b96935014502a18
Andy Wilson <andy.wilson1234@gmail.com>
2025-09-17 22:39:02 +0100

    Create childWorker.ts

---
+++ packages/server/src/childWorker.ts
@@ -0,0 +1,9 @@
+process.on("message", async (msg) => {
+  const { url } = msg;
+
+  // Example SSR logic
+  const { renderPage } = require("./renderer");
+  const html = await renderPage(url);
+
+  process.send({ html });
+});
