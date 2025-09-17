d4cc885404bb7d5f829e9ffd7b65dd2a36bcaa77
Andy Wilson <andy.wilson1234@gmail.com>
2025-09-17 23:21:01 +0100

    Update devChildWorker.ts

--- packages/server/src/devChildWorker.ts
+++ packages/server/src/devChildWorker.ts
@@ -1,7 +1,23 @@
-process.on("message", async (msg) => {
-  const { url } = msg;
-  const { renderPage } = require("./devRenderer");
-  const html = await renderPage(url);
+import tsNode from "ts-node";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-  process.send({ html });
+// Enable ts-node to run TSX directly
+tsNode.register({ transpileOnly: true, compilerOptions: { module: "commonjs", jsx: "react-jsx" } });
+
+process.on("message", async (msg) => {
+  const { file, url } = msg;
+
+  try {
+    // Dynamically import page component
+    const Page = require(file).default;
+
+    // Render React component to HTML
+    const html = ReactDOMServer.renderToString(React.createElement(Page, { url }));
+
+    process.send({ html });
+  } catch (err) {
+    console.error("SSR Child error:", err);
+    process.send({ html: "<h1>Error rendering page</h1>" });
+  }
 });
